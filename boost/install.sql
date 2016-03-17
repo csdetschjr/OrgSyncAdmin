1009b2310921a23358ff05cb6d4177546975bbf7
Chris Detsch <cd62936@email.appstate.edu>
2016-03-17 16:08:48 -0400

    Somewhere along the way the inserts for the umbrellas was removed, this commit adds them back in.

--- boost/install.sql
+++ boost/install.sql
@@ -9,6 +9,11 @@ CREATE TABLE appsync_umbrella (
 
 CREATE SEQUENCE appsync_umbrella_seq;
 
+INSERT INTO appsync_umbrella (id, name, orgsync_id) VALUES (nextval('appsync_umbrella_seq'), 'CSIL', 87895);
+INSERT INTO appsync_umbrella (id, name, orgsync_id) VALUES (nextval('appsync_umbrella_seq'), 'Orientation', 112682);
+INSERT INTO appsync_umbrella (id, name, orgsync_id) VALUES (nextval('appsync_umbrella_seq'), 'Student Development', 101226);
+INSERT INTO appsync_umbrella (id, name, orgsync_id) VALUES (nextval('appsync_umbrella_seq'), 'University Housing', 107843);
+
 CREATE TABLE appsync_umbrella_admin (
     id INT NOT NULL,
     username VARCHAR NOT NULL,
